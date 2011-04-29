05bb03c20c7ab93d9445efcb9efc474596039973
stef <stefan.marsiske@gmail.com>
2011-04-29 20:54:42 +0200

    [enh] form js is now in extra file

---
+++ media/js/form.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+                    $(".fieldWrapper").hide();
+                    $('.help_text').hide();
+                    $(".fieldWrapper").hover(function(event) {$(this).find('.help_text').toggle();});
+                    $("#show_optionals").hide();
+                    $("#id_country").parent().show();
+                    $("#save_button").hide();
+                    $("#id_contractual").click(function(event) {
+                                                 $("#id_contract_excerpt_parent").parent().toggle();
+                                               });
+                    $("#show_optionals").click(function(event) {
+                                                 $(this).hide();
+                                                 $("#optional .fieldWrapper").show();
+                                                 $("#id_contract_excerpt_parent").parent().hide();
+                                               });
+                    $('#id_country').change(function() {
+                                              var country=$(this).attr('value');
+                                              if(country.length>0) {
+                                                $('#id_operator').parent().show();
+                                                $.getJSON('/ajax/'+country, function(data) {
+                                                            $("#id_operator").autocomplete(data, { minChars: 0, autoFill: true, });
+                                                          });
+                                              }
+                                            });
+                    $('#id_operator').change(function() {
+                                               var country=$('#id_country').attr('value');
+                                               var operator=$(this).val();
+                                               if(operator.length>0) {
+                                                 $('#id_contract').parent().show();
+                                                 $.getJSON('/ajax/'+country+'/'+operator, function(data) {
+                                                             $("#id_contract").autocomplete(data,{ minChars: 0, });
+                                                           });
+                                               }
+                                             });
+                    $('#id_contract').change(function() {
+                                               if($(this).val().length>0) {
+                                                 $('#id_comment').parent().show();
+                                                 $('#save_button').show();
+                                                 $("#show_optionals").show();
+                                               }
+                                             });
+                  });
