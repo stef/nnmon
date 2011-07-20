4c5ebdee99cb905eac407a48568b302ca1bd624a
stef <stefan.marsiske@gmail.com>
2011-07-20 16:42:35 +0200

    [enh] added design

--- settings.py
+++ settings.py
@@ -140,6 +140,14 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = ("django.contrib.auth.context_processors.auth",
+                               "django.core.context_processors.debug",
+                               "django.core.context_processors.i18n",
+                               "django.core.context_processors.media",
+                               "django.core.context_processors.static",
+                               'django.core.context_processors.request',
+                               "django.contrib.messages.context_processors.messages")
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error.
