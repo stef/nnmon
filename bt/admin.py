9c0c6d9e38a8e324dd521b1753267a87bd075a31
stef <stefan.marsiske@gmail.com>
2011-09-26 23:24:04 +0200

    [enh] improved admin listing for objects

--- bt/admin.py
+++ bt/admin.py
@@ -2,19 +2,22 @@ from django.contrib import admin
 from bt import models
 
 class ViolationAdmin(admin.ModelAdmin):
-    pass
+   list_display = ('country', 'operator', 'contract', 'resource_name', 'media', 'activationid')
+   list_filter = ('operator', 'contract', 'resource_name', 'media', 'country')
 admin.site.register(models.Violation, ViolationAdmin)
 
 class CommentAdmin(admin.ModelAdmin):
-    pass
+   list_display = ('violation', 'submitter_name', 'comment')
+   list_filter = ('violation', 'submitter_name')
 admin.site.register(models.Comment, CommentAdmin)
 
 class AttachmentAdmin(admin.ModelAdmin):
-    pass
+   list_display = ('name', 'comment')
 admin.site.register(models.Attachment, AttachmentAdmin)
 
 class ConfirmationAdmin(admin.ModelAdmin):
-    pass
+   list_display = ('violation', 'key')
+   list_filter = ('violation__operator', 'violation__contract', 'violation__resource_name', 'violation__media', 'violation__country')
 admin.site.register(models.Confirmation, ConfirmationAdmin)
 
 class FeaturedCaseAdmin(admin.ModelAdmin):
